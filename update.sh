10375090f58d278d8272d622eef0cc761305eb1b
Camilo Cabrera <cecabrera55@gmail.com>
2017-12-21 11:55:11 -0500

    update

---
+++ update.sh
@@ -0,0 +1,10 @@
+cd pdt_dnp/
+git fetch --all
+git reset --hard origin/master
+cd ..
+
+sudo rm -R /srv/shiny-server/pdt_dnp/
+sudo cp -R pdt_dnp/ /srv/shiny-server/
+sudo chmod 777 -R /srv/shiny-server/pdt_dnp/
+
+# check logs: sudo cat /var/log/shiny-server/pdt_dnp-shiny
