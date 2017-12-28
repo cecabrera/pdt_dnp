f03591a169075472899bf29eaabf0c0954ffbf22
Camilo Cabrera <cecabrera55@gmail.com>
2017-12-27 20:19:54 -0500

    Actualización externo

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ cd ..
 sudo rm -R /srv/shiny-server/pdt_dnp/
 sudo cp -R pdt_dnp/ /srv/shiny-server/
 sudo chmod 777 -R /srv/shiny-server/pdt_dnp/
+sudo reboot now
 
 # check logs: sudo cat /var/log/shiny-server/pdt_dnp-shiny
 # start rstudio-server: sudo service rstudio-server start
